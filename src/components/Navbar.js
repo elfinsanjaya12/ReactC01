f3f5f0565ec0e74b42e45c015384a1181329cedb
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2019-02-06 17:10:43 +0700

    Perbaikan Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,6 @@
 import React, { Component } from "react";
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  Button
-} from "reactstrap";
 import logo from "../assets/images/logo.png";
+import keranjang from "../assets/images/icon/keranjang.png";
 
 class Header extends Component {
   constructor(props) {
@@ -29,35 +20,95 @@ class Header extends Component {
   render() {
     return (
       <div>
-        <Navbar
-          color="white"
-          light
-          expand="md"
-          className="border py-1 sticky-top shadow-sm p-1 mb-5 bg-white rounded"
-        >
-          <NavbarBrand href="/">
-            <img
-              src={logo}
-              alt="Logo"
-              className="d-inline-block align-center pr-2"
-              height="50"
-            />
-            Gama.Textile
-          </NavbarBrand>
-          <NavbarToggler onClick={this.toggle} />
-          <Collapse isOpen={this.state.isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Kategori</NavLink>
-              </NavItem>
-              <NavItem>
-                <Button outline color="success" href="/">
-                  Masuk/Daftar
-                </Button>
-              </NavItem>
-            </Nav>
-          </Collapse>
-        </Navbar>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light border py-1 sticky-top shadow-sm p-1 mb-5 bg-white rounded">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="#">
+              <img
+                src={logo}
+                height="36"
+                alt="Logo"
+                className="d-inline-block align-center pr-2"
+              />
+              Gama Textile
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon" />
+            </button>
+            <div
+              className="collapse navbar-collapse"
+              id="navbarSupportedContent"
+            >
+              {/* kanan */}
+              <ul className="navbar-nav rm-auto">
+                <li>
+                  <a className="nav-link" href="#">
+                    Kategori <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+              </ul>
+              {/* akhir kanan */}
+
+              {/* tengah */}
+              <div className="container">
+                <div className="row">
+                  <div className="col-sm-12">
+                    <input
+                      className="form-control"
+                      type="search"
+                      placeholder="Search"
+                      aria-label="Search"
+                    />
+                  </div>
+                </div>
+              </div>
+              {/* akhir tengah */}
+              {/* kiri */}
+              <ul className="navbar-nav ml-auto">
+                <li>
+                  <a className="nav-link" href="#">
+                    <img
+                      src={keranjang}
+                      height="22"
+                      alt="Logo"
+                      className="d-inline-block align-center pr-2"
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a className="nav-link" href="#">
+                    |
+                  </a>
+                </li>
+
+                <div className="container">
+                  <li>
+                    <a className="nav-link" href="#">
+                      Masuk
+                    </a>
+                  </li>
+                </div>
+                <div className="container">
+                  <li>
+                    <button
+                      className="btn btn-outline-success my-2 my-sm-0"
+                      type="submit"
+                    >
+                      Daftar
+                    </button>
+                  </li>
+                </div>
+              </ul>
+            </div>
+          </div>
+        </nav>
       </div>
     );
   }
